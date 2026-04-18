c92487c7a260cd5b4feb87459e8f76fad73f304a
NicoMantas <nicolasmantilla15062015@gmail.com>
2026-04-18 18:43:25 -0500

    Politicas de privacidad

--- ShopList_Project/app/src/main/java/com/upb/shoplist/ShoppingList.kt
+++
@@ -1,8 +0,0 @@
-package com.upb.shoplist
-
-data class ShoppingList(
-    val id: String,
-    val name: String,
-    val products: List<Product>,
-    val createdAt: Long = System.currentTimeMillis()
-)
